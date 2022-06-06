c62f30d0761f8bdf3837803edcf759b246794cc0
Ankur2002 <ankurchaurasia002@gmail.com>
2022-06-06 11:41:20 +0530

    done

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://food-recipes-assignment.herokuapp.com',
   headers: {
     Authentication: localStorage.getItem('token'),
   },
